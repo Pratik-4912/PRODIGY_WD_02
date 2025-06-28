e9d1e2077ae2953e25d96555790b5535e00d6469
Pratik-4912 <pratik8019@gmail.com>
2025-06-28 13:48:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let [hours, minutes, seconds] = [0, 0, 0];
+let display = document.getElementById("display");
+let timer = null;
+
+function stopwatch() {
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+
+  display.innerHTML = `${h}:${m}:${s}`;
+}
+
+function start() {
+  if (timer !== null) return;
+  timer = setInterval(stopwatch, 1000);
+}
+
+function pause() {
+  clearInterval(timer);
+  timer = null;
+}
+
+function reset() {
+  clearInterval(timer);
+  timer = null;
+  [hours, minutes, seconds] = [0, 0, 0];
+  display.innerHTML = "00:00:00";
+}
+
